33aaf6c752c4336f85d8767568bd9ad99cdb61b6
r0maps <romapailhez@gmail.com>
2025-02-23 22:41:11 +0100

    Add files via upload

---
+++ HarlequinMalverkPack-main/main.lua
@@ -0,0 +1,28 @@
+--- STEAMODDED HEADER
+--- MOD_NAME: HarlequinMalverkPack
+--- MOD_ID: harlequinmalverk
+--- MOD_AUTHOR: [Pepperfaced]
+--- MOD_DESCRIPTION: Balatro Harlequin Texture Pack
+--- PREFIX: harl
+--- VERSION: 1.0.0
+--- DEPENDENCIES: [malverk]
+
+AltTexture{
+    key = 'joker',
+    set = 'Joker',
+    path = 'HarlequinMalverkPack_joker.png',
+}
+
+TexturePack{
+    key = 'harl',
+    textures = {
+        'harl_joker'
+    },
+    loc_txt = {
+        name = "Harlequin Textures",
+        text = {
+            "Changes Jokers to",
+            "Harlequin cards"
+        }
+    }
+}
